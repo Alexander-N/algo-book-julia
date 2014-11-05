03afc536003dcc8f91b93947138aeec6b57c2b3d
a <A.Niederbuehl@gmail.com>
2014-11-05 14:27:21 +0100

    sort and compare

---
+++ Analysis/anagramSolution2.jl
@@ -0,0 +1,9 @@
+function anagramSolution2(string1, string2)
+    sort!(string1.data)
+    sort!(string2.data)
+    return string1 == string2
+end
+
+println(anagramSolution2("abcd","dcba"))
+println(anagramSolution2("abcd","cdba"))
+println(anagramSolution2("abcd","cdbi"))
