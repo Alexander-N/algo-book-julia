b5387a0f507443b0abe0572043c60e0e6f743c4e
a <A.Niederbuehl@gmail.com>
2014-11-06 13:51:47 +0100

    fix multiple char bug

--- Analysis/anagramSolution1.jl
+++ Analysis/anagramSolution1.jl
@@ -1,16 +1,16 @@
 using Base.Test
 
 function anagramSolution1(string1, string2)
-    #this function is incorrect for multiple occurences of a character 
-    
     if length(string1) != length(string2)
         return false
     end
+    string2 = [char for char in string2]
     for s1 in string1
         found = false
-        for s2 in string2
-            if s1 == s2
+        for i in 1:length(string2) 
+            if s1 == string2[i]
                 found = true
+                string2[i] = ' ' 
             end
         end
         if !found
