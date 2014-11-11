d01ee52684f6b00fa279fa70e02f2a10885e0c9c
a <A.Niederbuehl@gmail.com>
2014-11-11 08:09:33 +0100

    balanced parentheses

---
+++ Basic-Data-Structures/parcheck1.jl
@@ -0,0 +1,26 @@
+include("stack.jl")
+using myStack
+using Base.Test
+
+function parChecker(symbolString::ASCIIString)
+    s = Stack()
+    for symbol in symbolString
+        if symbol == '('
+            push!(s, symbol)
+        else
+            if isempty(s)
+                return false
+            else
+                pop!(s)
+            end
+        end
+    end
+    if isempty(s)
+        return true
+    else
+        return false
+    end
+end
+
+@test parChecker("((()))") == true
+@test parChecker("(()") == false
