58cd03d384e154e822faad0e3c686a3e3c6623d4
a <A.Niederbuehl@gmail.com>
2014-11-13 11:27:31 +0100

    converting from decimal to binary

---
+++ Basic-Data-Structures/divideBy2.jl
@@ -0,0 +1,22 @@
+include("stack.jl")
+using myStack
+using Base.Test
+
+function divideBy2(decNumber::Int64)
+    remstack = Stack()
+
+    while decNumber > 0
+        decNumber, remainder = divrem(decNumber, 2)
+        push!(remstack,remainder)
+    end
+
+    binString = ""
+    while !isempty(remstack)
+        binString = binString * string(pop!(remstack))
+    end
+    
+    return binString 
+end
+
+
+@test divideBy2(42) == "101010"
