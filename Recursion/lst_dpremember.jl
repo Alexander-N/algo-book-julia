fe27110344f60f2ac18ab8121015022e77532208
Alexander-N <A.Niederbuehl@gmail.com>
2015-01-27 07:55:31 +0100

    Complete Solution to the Change Problem (dynamic programming)

--- Recursion/lst_dpremember.jl
+++ Recursion/lst_dpremember.jl
@@ -14,10 +14,25 @@ function dpMakeChange(coinValueList, change, minCoins, coinsUsed)
     return minCoins[change+1]
 end
 
-amnt = 63
-clist = [1,5,10,21,25]
-coinsUsed = zeros(Int,amnt+1)
-coinCount = zeros(Int,amnt+1)
+function printCoins(coinsUsed, change)
+    coin = change
+    while change > 0
+        thisCoin = coinsUsed[change+1]
+        println(thisCoin)
+        change = change - thisCoin
+    end
+end
 
+function main()
+    amnt = 63
+    clist = [1,5,10,21,25]
+    coinsUsed = zeros(Int,amnt+1)
+    coinCount = zeros(Int,amnt+1)
+    println("Making change for $amnt requires")
+    println(string(dpMakeChange(clist, amnt, coinCount, coinsUsed), " coins"))
+    printCoins(coinsUsed, amnt)
+    println("The used list is as follows:")
+    print(coinsUsed)
+end
 
-print(dpMakeChange(clist,amnt,coinCount,coinsUsed))
+main()
