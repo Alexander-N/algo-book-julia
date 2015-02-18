995da3d675d4566d15c606e4d5f688c5ad1a831b
Alexander-N <A.Niederbuehl@gmail.com>
2015-02-18 23:48:52 +0100

    hash, rehash

--- Sorting-and-Searching/HashTable.jl
+++ Sorting-and-Searching/HashTable.jl
@@ -4,3 +4,12 @@ type HashTable
     data::Array{Any, 1}
     HashTable() = new(11,int(zeros(11)),zeros(11))
 end
+
+function hashfunction(key, size)
+    key % size
+end
+
+function rehash(oldhash, size)
+    (oldhash+1) % size
+end
+
